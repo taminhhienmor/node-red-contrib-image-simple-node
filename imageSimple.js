ccd492d22cff4af39f3b59d365ccefdb7b0b25de
taminhhienmor <56866116+taminhhienmor@users.noreply.github.com>
2020-05-07 11:05:39 +0900

    update get image local

--- imageSimple.js
+++ imageSimple.js
@@ -1,4 +1,5 @@
 var Jimp = require('jimp');
+var fs = require("fs");
 module.exports = function (RED) {
 	function imageSimple(config) {
 		"use strict";
@@ -34,6 +35,9 @@ module.exports = function (RED) {
 		// original
 		var original = config.original
 
+		// local file
+		var localfile = config.localfile
+
 		// clear image
 		RED.comms.publish("image_png", { id: this.id, imageData: "" });
 
@@ -64,69 +68,144 @@ module.exports = function (RED) {
 			}
 
 			msg.localFilename = msg.localFilename || 'image_edit.png'
-			try {
-				if (original) {
-					Jimp.read(image, (err, img) => {
-						if (err) {
-							node.error(RED._("Image not found!"),msg)
-							node.status({fill:"red",shape:"ring",text:"image invalid"});        
-							return;
-						};
-						img
-						.resize(img.bitmap.width, img.bitmap.height)					
-						.write(msg.localFilename)
-						.getBase64(Jimp.MIME_PNG, (errPng, pngData) => {
-							if (errPng) {
-								node.error(RED._("Error getting base64 image"),msg)
-								node.status({fill:"red",shape:"ring",text:"image invalid"});        
-								return;
-							}								
-							RED.comms.publish("image_png", { id: this.id, imageData: pngData });
-						});
-					});
-				} else {
-					Jimp.read(image, (err, img) => {
-						if (err) {
-							node.error(RED._("Image not found!"),msg)
-							node.status({fill:"red",shape:"ring",text:"image invalid"});        
-							return;
-						};
-						if (autoresize) {
-							height = img.bitmap.height / (img.bitmap.width / width);
-						}
-						img
-						.resize(width, height)
-						.rotate(rotate)
-						if(!cropFlag) {
-							if (cropX + cropWidth > width) {
-								node.error(RED._("cropX and cropWidth are larger width(" + width + ")!"),msg)
-								node.status({fill:"red",shape:"ring",text:"crop invalid"});        
-								return;
-							} else if (cropY + cropHeight > height ) {
-								node.error(RED._("cropY and cropHeight are larger height(" + height + ")!"),msg)
-								node.status({fill:"red",shape:"ring",text:"crop invalid"});        
-								return;
-							} else {
-								img.crop(cropX, cropY, cropWidth, cropHeight)
-							}
-						}
-						img.flip(flipX, flipY)
-						.write(msg.localFilename)
-						.getBase64(Jimp.MIME_PNG, (errPng, pngData) => {
-							if (errPng) {
-								node.error(RED._("Error getting base64 image"),msg)
-								node.status({fill:"red",shape:"ring",text:"image invalid"});        
-								return;
-							}						
-							RED.comms.publish("image_png", { id: this.id, imageData: pngData });
-						});
-					});
+			var idNow = this.id
+
+			if (localfile) {
+				var linkLocalFile = image
+				try {
+					if (original) {
+						fs.readFile(linkLocalFile, (err, image) => {
+							Jimp.read(image, (err, img) => {
+								if (err) {
+									node.error(RED._("Image not found!"),msg)
+									node.status({fill:"red",shape:"ring",text:"image invalid"});        
+									return;
+								};
+								img
+								.resize(img.bitmap.width, img.bitmap.height)					
+								.write(msg.localFilename)
+								.getBase64(Jimp.MIME_PNG, (errPng, pngData) => {
+									if (errPng) {
+										node.error(RED._("Error getting base64 image"),msg)
+										node.status({fill:"red",shape:"ring",text:"image invalid"});        
+										return;
+									}								
+									RED.comms.publish("image_png", { id: idNow, imageData: pngData });
+								});
+							});
+							
+						})
+					} else {
+						fs.readFile(linkLocalFile, (err, image) => {
+							Jimp.read(image, (err, img) => {
+								if (err) {
+									node.error(RED._("Image not found!"),msg)
+									node.status({fill:"red",shape:"ring",text:"image invalid"});        
+									return;
+								};
+								if (autoresize) {
+									height = img.bitmap.height / (img.bitmap.width / width);
+								}
+								img
+								.resize(width, height)
+								.rotate(rotate)
+								if(!cropFlag) {
+									if (cropX + cropWidth > width) {
+										node.error(RED._("cropX and cropWidth are larger width(" + width + ")!"),msg)
+										node.status({fill:"red",shape:"ring",text:"crop invalid"});        
+										return;
+									} else if (cropY + cropHeight > height ) {
+										node.error(RED._("cropY and cropHeight are larger height(" + height + ")!"),msg)
+										node.status({fill:"red",shape:"ring",text:"crop invalid"});        
+										return;
+									} else {
+										img.crop(cropX, cropY, cropWidth, cropHeight)
+									}
+								}
+								img.flip(flipX, flipY)
+								.write(msg.localFilename)
+								.getBase64(Jimp.MIME_PNG, (errPng, pngData) => {
+									if (errPng) {
+										node.error(RED._("Error getting base64 image"),msg)
+										node.status({fill:"red",shape:"ring",text:"image invalid"});        
+										return;
+									}						
+									RED.comms.publish("image_png", { id: idNow, imageData: pngData });
+								});
+							});
+						})
+					}
+					node.send(msg);
+				} catch (error) {
+					node.error(RED._("Error getting image"),msg)
+					node.status({fill:"red",shape:"ring",text:"image invalid"});        
+					return;
 				}
-				node.send(msg);
-			} catch (error) {
-				node.error(RED._("Error getting image"),msg)
-				node.status({fill:"red",shape:"ring",text:"image invalid"});        
-				return;
+			} else {
+				try {
+					if (original) {
+						Jimp.read(image, (err, img) => {
+							if (err) {
+								node.error(RED._("Image not found!"),msg)
+								node.status({fill:"red",shape:"ring",text:"image invalid"});        
+								return;
+							};
+							img
+							.resize(img.bitmap.width, img.bitmap.height)					
+							.write(msg.localFilename)
+							.getBase64(Jimp.MIME_PNG, (errPng, pngData) => {
+								if (errPng) {
+									node.error(RED._("Error getting base64 image"),msg)
+									node.status({fill:"red",shape:"ring",text:"image invalid"});        
+									return;
+								}								
+								RED.comms.publish("image_png", { id: this.id, imageData: pngData });
+							});
+						});
+					} else {
+						Jimp.read(image, (err, img) => {
+							if (err) {
+								node.error(RED._("Image not found!"),msg)
+								node.status({fill:"red",shape:"ring",text:"image invalid"});        
+								return;
+							};
+							if (autoresize) {
+								height = img.bitmap.height / (img.bitmap.width / width);
+							}
+							img
+							.resize(width, height)
+							.rotate(rotate)
+							if(!cropFlag) {
+								if (cropX + cropWidth > width) {
+									node.error(RED._("cropX and cropWidth are larger width(" + width + ")!"),msg)
+									node.status({fill:"red",shape:"ring",text:"crop invalid"});        
+									return;
+								} else if (cropY + cropHeight > height ) {
+									node.error(RED._("cropY and cropHeight are larger height(" + height + ")!"),msg)
+									node.status({fill:"red",shape:"ring",text:"crop invalid"});        
+									return;
+								} else {
+									img.crop(cropX, cropY, cropWidth, cropHeight)
+								}
+							}
+							img.flip(flipX, flipY)
+							.write(msg.localFilename)
+							.getBase64(Jimp.MIME_PNG, (errPng, pngData) => {
+								if (errPng) {
+									node.error(RED._("Error getting base64 image"),msg)
+									node.status({fill:"red",shape:"ring",text:"image invalid"});        
+									return;
+								}						
+								RED.comms.publish("image_png", { id: this.id, imageData: pngData });
+							});
+						});
+					}
+					node.send(msg);
+				} catch (error) {
+					node.error(RED._("Error getting image"),msg)
+					node.status({fill:"red",shape:"ring",text:"image invalid"});        
+					return;
+				}
 			}
 		});
 	}
